68907617d90b50f2a420884bf62ac5737391fc3f
konstantinosKatsamis <75335809+konstantinosKatsamis@users.noreply.github.com>
2023-04-05 00:00:12 +0300

    changing activities
    Implementation needed

---
+++ app/src/main/java/com/example/mysignupapp/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.mysignupapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.content.Intent;
+import android.widget.Button;
+
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
+    }
+
+    public void openActivity2() {
+        Intent intent = new Intent(this, Register.class);
+        startActivity(intent);
+    }
+}
