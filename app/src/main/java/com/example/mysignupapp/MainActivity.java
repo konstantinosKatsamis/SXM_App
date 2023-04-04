2df1be6a5db6fe8d9fcdd0cd2721ff7383031996
konstantinosKatsamis <75335809+konstantinoskatsamis@users.noreply.github.com>
2023-04-05 00:10:38 +0300

    test

--- app/src/main/java/com/example/mysignupapp/MainActivity.java
+++ app/src/main/java/com/example/mysignupapp/MainActivity.java
@@ -2,34 +2,59 @@ package com.example.mysignupapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
-import android.content.Intent;
 import android.widget.Button;
 
-import com.google.android.material.button.MaterialButton;
-
-public class MainActivity extends AppCompatActivity {
-    private Button button;
+public class MainActivity extends AppCompatActivity
+{
+    Button button_for_login;
+    Button button_for_register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        button_for_login = findViewById(R.id.completeLogin);
+        button_for_register = findViewById(R.id.register_button);
+
+        button_for_register.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                openActivity2();
+            public void onClick(View ownerView) {
+
+                Intent intent_for_register = new Intent(MainActivity.this, Register.class);
+                startActivity(intent_for_register);
+
             }
         });
     }
 
-    public void openActivity2() {
-        Intent intent = new Intent(this, Register.class);
-        startActivity(intent);
+    @Override
+    public void onBackPressed()
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.app_name);
+        builder.setIcon(R.mipmap.ic_launcher);
+        builder.setMessage("Are you sure you want to exit?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        moveTaskToBack(true);
+                        android.os.Process.killProcess(android.os.Process.myPid());
+                        System.exit(1);
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
+
     }
 }
